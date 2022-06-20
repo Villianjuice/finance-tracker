75282fbc0983ca1b1b14bc59f20ccf14538a1023
Aslan <tavorktar@gmail.com>
2022-06-20 20:31:00 +0600

    fix auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Navbar } from './components';
+import { useAuthContext } from './context/AuthContext';
+import { auth } from './firebase/config';
 import { Home, Login, SignUp } from './pages';
 
 // import './App.css';
 
 function App() {
+  const {dispatch} = useAuthContext()
+
+  useEffect(() => {
+    const cancel = onAuthStateChanged(auth ,(_user) => {
+      dispatch({type: 'LOGIN', payload: _user})
+    })
+
+    return () => cancel()
+  }, [])
+
   return (
     <div className="App">
       <Navbar />
